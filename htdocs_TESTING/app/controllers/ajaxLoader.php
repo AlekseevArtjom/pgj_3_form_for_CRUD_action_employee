52aeee2e8afd4e9c6ae78c941861db080dbe03e6
AlekseevArtjom <135531758+AlekseevArtjom@users.noreply.github.com>
2025-01-25 14:21:58 +0700

    Update ajaxLoader.php

--- htdocs_TESTING/app/controllers/ajaxLoader.php
+++ htdocs_TESTING/app/controllers/ajaxLoader.php
@@ -4,12 +4,12 @@ if( !defined('BASE_DIR')) define('BASE_DIR',$_SERVER['DOCUMENT_ROOT']);
 if( !defined('PS')) define('PS', PATH_SEPARATOR);
 
 set_include_path(get_include_path().
-									PS.BASE_DIR.
-									PS.BASE_DIR.DS.'app'.
-									PS.BASE_DIR.DS.'app'.DS.'config'.
-									PS.BASE_DIR.DS.'app'.DS.'controllers'.
-									PS.BASE_DIR.DS.'app'.DS.'models'.
-									PS.BASE_DIR.DS.'app'.DS.'views');
+		PS.BASE_DIR.
+		PS.BASE_DIR.DS.'app'.
+		PS.BASE_DIR.DS.'app'.DS.'config'.
+		PS.BASE_DIR.DS.'app'.DS.'controllers'.
+		PS.BASE_DIR.DS.'app'.DS.'models'.
+		PS.BASE_DIR.DS.'app'.DS.'views');
 									
 require_once('baseController.php');										
 require_once('baseAjaxController.php');
